53c6f65b6bade52a6016b121d4f84e8f53f5f12e
Harrison Wekesa <harrisonwekesa09@gmail.com>
2021-07-25 12:46:51 +0300

    activity detector

--- activity_recognition.py
+++ activity_recognition.py
@@ -2,18 +2,17 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-%matplotlib inline
+#matplotlib inline
 import warnings
 warnings.filterwarnings("ignore")
 
-train = pd.read_csv("train.csv")
+train = pd.read_csv("train-1.csv")
 test = pd.read_csv("test.csv")
 
 train['Data'] = 'Train'
 test['Data'] = 'Test'
 both = pd.concat([train, test], axis=0).reset_index(drop=True)
 both['subject'] = '#' + both['subject'].astype(str)
-
 train.shape, test.shape
 #Output ((7352, 564), (2947, 564))
 
@@ -40,20 +39,20 @@ Subject = both['subject']
 train = both.copy()
 train = train.drop(['Data','subject','Activity'], axis =1)
 
-###To Scale the data
+#To Scale the data
 # Standard Scaler
 from sklearn.preprocessing import StandardScaler
 slc = StandardScaler()
 train = slc.fit_transform(train)
-​
+
 # dimensionality reduction
 from sklearn.decomposition import PCA
 pca = PCA(n_components=0.9, random_state=0)
 train = pca.fit_transform(train)
-##Splitting the  data into training and testing
+#Splitting the  data into training and testing
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(train, activity, test_size = 0.2, random_state = 0)
-##Test options and evaluation metric
+#Test options and evaluation metric
 
 num_folds = 10
 seed = 0
@@ -61,23 +60,21 @@ scoring = 'accuracy'
 results = {}
 accuracy = {}
 
-##Activity Recognition Algorithm
-##Finalizing the model and comparing the test, predict results
+#Activity Recognition Algorithm
+#Finalizing the model and comparing the test, predict results
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
 from sklearn.model_selection import KFold, cross_val_score
 model = KNeighborsClassifier(algorithm= 'auto', n_neighbors= 8, p= 1, weights= 'distance')
-​
+
 _ = cross_val_score(model, X_train, y_train, cv=10, scoring=scoring)
 results["GScv"] = (_.mean(), _.std())
-​
+
 model.fit(X_train, y_train) 
 y_predict = model.predict(X_test)
-​
+
 accuracy["GScv"] = accuracy_score(y_test, y_predict)
-​
-print(classification_report(y_test, y_predict))
-​
+print(classification_report(y_test, y_predict), end = '\n')
 cm= confusion_matrix(y_test, y_predict)
 sns.heatmap(cm, annot=True)
 
